8a2ff6cd12a26ec05e20c8377c4ed59f2574bf4d
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2023-10-24 00:10:05 +0400

    AdminPanelLayoutDividePartials

---
+++ HDF.PresentationLayer.Backend/ViewComponents/ScriptsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HDF.PresentationLayer.Backend.ViewComponents
+{
+    public class ScriptsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
--- HDF.PresentationLayer.Backend/ViewModels/Account/LoginVM.cs
+++ HDF.PresentationLayer.Backend/ViewModels/Account/LoginVM.cs
@@ -1,8 +1,11 @@
-﻿namespace HDF.PresentationLayer.Backend.ViewModels.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HDF.PresentationLayer.Backend.ViewModels.Account
 {
     public class LoginVM
     {
         public string Username { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public bool Check { get; set; }
     }
